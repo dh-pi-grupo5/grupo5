195e17d2825fd445008e3728a56e901e70d23670
MarcoMoutsopoulos <marcomoutsopoulos@yahoo.com.br>
2022-11-21 00:20:49 -0300

    rotas p/home,login,product,admClient,shoppingCart

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ const app = express();
 app.use(express.static('public'));
 
 //Criando a rota para home
-app.get('/home', (req,res)=>{
+app.get('/', (req,res)=>{
     return res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/login.html', (req,res)=>{
+    return res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/product.html', (req,res)=>{
+    return res.sendFile(__dirname + '/views/product.html');
+});
+app.get('/admClient.html', (req,res)=>{
+    return res.sendFile(__dirname + '/views/admClient.html');
+});
+app.get('/shoppingCart.html', (req,res)=>{
+    return res.sendFile(__dirname + '/views/shoppingCart.html');
 });
 
 app.listen(3000);
